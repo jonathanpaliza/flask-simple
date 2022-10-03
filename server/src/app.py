9cc6903d43eb871daa59b1037b22ebc126cb6b25
jonathanpaliza <jonathanpaliza.devopsgames@gmail.com>
2022-10-03 11:26:11 +0800

    Update app.py

--- server/src/app.py
+++ server/src/app.py
@@ -17,6 +17,10 @@ def index():
     response.headers["Cache-Control"] = "public, max-age=300, s-maxage=600"
     return response
 
+
+def test():
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
